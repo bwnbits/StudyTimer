a3de4ccbb6b2e0fa14df16dd37d8a434e5a779d2
Abhishek Ruhela <bwnbits@gmail.com>
2026-08-23 17:50:36 +0530

    Initial Commit

---
+++ StudyTimer/StudyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTimerApp.swift
+//  StudyTimer
+//
+//  Created by A.R. on 23/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StudyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
